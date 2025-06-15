64f5814e8821d8f4ca4062d0f90de4b9e50f7533
Egor Dovgalev <egor.dovgalev.anthill@gmail.com>
2025-06-15 23:08:40 +0300

    fix

--- src/app/components/Paginator/Paginator.tsx
+++ src/app/components/Paginator/Paginator.tsx
@@ -7,7 +7,7 @@ export default function Paginator({ total }: { total: number }) {
   const searchParams = useSearchParams();
   const router = useRouter();
 
-  const currentPage = Number(searchParams.get("page") || null);
+  const currentPage = Number(searchParams.get("page") || 1);
 
   const updatePage = (page: string) => {
     if (page !== "..." && Number(page) !== currentPage) {
@@ -48,74 +48,68 @@ export default function Paginator({ total }: { total: number }) {
   const pages = getPageNumbers();
 
   return (
-    <>
-      {currentPage === null ? null : (
-        <nav>
-          <ul className="fixed bottom-2 right-[50%] translate-x-[50%] flex items-center -space-x-px h-10 text-base">
-            <li
-              onClick={() => updatePage(String(currentPage - 1 || 1))}
-              className="cursor-pointer flex items-center justify-center px-2 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              <svg
-                className="w-3 h-3 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 1 1 5l4 4"
-                />
-              </svg>
-            </li>
+    <nav>
+      <ul className="fixed bottom-2 right-[50%] translate-x-[50%] flex items-center -space-x-px h-10 text-base">
+        <li
+          onClick={() => updatePage(String(currentPage - 1 || 1))}
+          className="cursor-pointer flex items-center justify-center px-2 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+        >
+          <svg
+            className="w-3 h-3 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 1 1 5l4 4"
+            />
+          </svg>
+        </li>
 
-            {pages.map((page, index) => {
-              return (
-                <li
-                  key={index}
-                  aria-current="true"
-                  onClick={() => updatePage(String(page))}
-                  className={` cursor-pointer flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
-                    page === currentPage ? "font-bold" : ""
-                  }`}
-                >
-                  {page}
-                </li>
-              );
-            })}
+        {pages.map((page, index) => {
+          return (
             <li
-              onClick={() =>
-                updatePage(
-                  currentPage + 1 > total
-                    ? String(total)
-                    : String(currentPage + 1)
-                )
-              }
-              className="cursor-pointer flex items-center justify-center px-2 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              key={index}
+              aria-current="true"
+              onClick={() => updatePage(String(page))}
+              className={` cursor-pointer flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
+                page === currentPage ? "font-bold" : ""
+              }`}
             >
-              <svg
-                className="w-3 h-3 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 9 4-4-4-4"
-                />
-              </svg>
+              {page}
             </li>
-          </ul>
-        </nav>
-      )}
-    </>
+          );
+        })}
+        <li
+          onClick={() =>
+            updatePage(
+              currentPage + 1 > total ? String(total) : String(currentPage + 1)
+            )
+          }
+          className="cursor-pointer flex items-center justify-center px-2 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+        >
+          <svg
+            className="w-3 h-3 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m1 9 4-4-4-4"
+            />
+          </svg>
+        </li>
+      </ul>
+    </nav>
   );
 }
